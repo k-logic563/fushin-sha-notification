846993010066ce4f455aefa889241174eee63c24
k-logic24 <k.log.ic563@gmail.com>
2022-11-08 09:14:40 +0900

    wip

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import json
-import boto3
 import requests
+import re
 from bs4 import BeautifulSoup
 
-bucket_name = 'fushinsha-log'
-object_key_name = 'data.json'
+headlines = []
+url_list = []
+address_list = []
 
-json_data = {
-    'data': [],
+d = {
+    '１':'一',
+    '２':'二',
+    '３':'三',
+    '４':'四',
+    '５':'五',
+    '６':'六',
+    '７':'七',
+    '８':'八',
+    '９':'九',
 }
+def get_address(url):
+    # 住所抽出
+    pattern = re.compile('^.*）(.*?)(付近)*で.*')
+    html = requests.get(url).content.decode('utf-8')
+    soup2 = BeautifulSoup(html, 'html.parser')
+    address = pattern.match(soup2.title.text).group(1)
+    #漢数字変換
+    pattern = re.compile('|'.join(d.keys()))
+    address = pattern.sub(lambda x: d[x.group()], address)
+    return address
 
-def lambda_handler(event, context):
-    # s3初期化
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-    obj = bucket.Object(object_key_name)
-    # データ取得
-    res = requests.get('https://fushinsha-joho.co.jp/search.cgi?pref=広島県')
-    soup = BeautifulSoup( res.text, 'html.parser' )
-    headlines = soup.find_all('a', class_='headline')
-    for link in headlines :
-        href = link.get('href')
-        title = link.text
-        payload = {
-            'href': href,
-            'title': title,
-        }
-        json_data['data'].append(payload)
-    # s3オブジェクトへ書き込み
-    r = obj.put(Body = json.dumps(json_data))
+# とりあえず広島県で固定する
+res = requests.get('https://fushinsha-joho.co.jp/search.cgi?pref=広島県')
+soup = BeautifulSoup( res.text, 'html.parser' )
+data = soup.find_all('a', class_='headline')
 
-    return {
-        'statusCode': 200,
-    }
+for i, row in enumerate(data) :
+    try :
+        headline = row.text.strip()
+        href = row.get('href')
+        # address = get_address(href)
+        
+        headlines.append(headline)
+        url_list.append(href)
+        # address_list.append(address)
+    except :
+        continue
+    
+print(headlines)

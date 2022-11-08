0f1a5e15c6da2eb31ed59f08b25c78d8ba0be154
k-logic24 <k.log.ic563@gmail.com>
2022-11-08 23:02:54 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -9,12 +9,27 @@ headline_list = []
 url_list = []
 address_list = []
 
+convert_number_props = {
+    '１':'一',
+    '２':'二',
+    '３':'三',
+    '４':'四',
+    '５':'五',
+    '６':'六',
+    '７':'七',
+    '８':'八',
+    '９':'九',
+}
+
 def get_address(url):
     # 住所抽出
     pattern = re.compile('^.*）(.*?)(付近)*で.*')
     html = requests.get(url).content.decode('utf-8')
     soup2 = BeautifulSoup(html, 'html.parser')
     address = pattern.match(soup2.title.text).group(1)
+    #漢数字変換
+    pattern = re.compile('|'.join(convert_number_props.keys()))
+    address = pattern.sub(lambda x: convert_number_props[x.group()], address)
     return address
 
 # とりあえず広島県で固定する
@@ -44,12 +59,13 @@ df_geo = pd.read_csv('japanese-addresses/latest.csv')
 df_geo_hiroshima = df_geo[df_geo.都道府県名 == '広島県']
 df_geo_hiroshima = df_geo_hiroshima.copy()
 df_geo_hiroshima.loc[:, 'address'] = df_geo_hiroshima.市区町村名 + df_geo_hiroshima.大字町丁目名
+
 # 緯度経度
 df_merge = df.merge(df_geo_hiroshima, on='address')
-pat = r"(?P<use>^.*）)(?P<nouse>\d+)"
-repl = lambda m: m.group('use').swapcase()
+pattern = r"(?P<use>^.*）)(?P<nouse>\d+)"
+replace = lambda m: m.group('use').swapcase()
 df_merge = df_merge.copy()
-df_merge.loc[:, 'headline'] = df_merge.headline.str.replace(pat, repl)
+df_merge.loc[:, 'headline'] = df_merge.headline.str.replace(pattern, replace)
 # マップ生成 広島県庁を中心に
 map = folium.Map(location=[34.39678032975633, 132.45960985216675], zoom_start = 10)
 
@@ -57,7 +73,7 @@ for i, r in df_merge.iterrows():
     folium.Marker(
         location = [r['緯度'], r['経度']],
         popup = folium.Popup(
-            f'<a href=" {r.url} "target="_blank"> {r.headline} </a>',
+            f'<a href=" { r.url } "target="_blank">{ r.headline }</a>',
             max_width = 300, min_width = 100
         )
     ).add_to(map)
